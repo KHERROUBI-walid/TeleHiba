c3c5db6f7908e24f808c02c82d93d7ae699df55b
Kherroubi_Walid <kherroubi.walid78@gmail.com>
2025-06-22 16:33:38 +0200

    Ajout de Turbo js

--- app_symfony/tailwind.config.js
+++ app_symfony/tailwind.config.js
@@ -1,11 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const plugin = require('tailwindcss-dir');
+
+module.exports = {
   content: [
-    './templates/**/*.html.twig',
-    './assets/**/*.js',
+    './templates/**/*.twig',
+    './assets/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [
+    plugin(), // Active les variantes dir:ltr et dir:rtl
+  ],
+  variants: {
+    extend: {
+      textAlign: ['dir'],
+      margin: ['dir'],
+      padding: ['dir'],
+      float: ['dir'],
+      space: ['dir'],
+      inset: ['dir'], // Pour left/right
+      borderRadius: ['dir'],
+      borderWidth: ['dir'],
+      divideWidth: ['dir'],
+      divideColor: ['dir'],
+    },
+  },
+}
